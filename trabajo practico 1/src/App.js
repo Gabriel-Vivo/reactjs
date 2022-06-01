3bf464d93a271f5857d45d0f935489d4491a7a6c
Gabriel Vivo <g.vivo@live.com>
2022-06-01 18:31:13 -0300

    trabajo practico 1

---
+++ trabajo practico 1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <h1> Bienvenido al curso de Reactjs</h1>
+     <p>Mi nombre es <b> Gabriel Vivo</b></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
